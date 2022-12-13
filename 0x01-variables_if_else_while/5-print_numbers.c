5c914b9d8fea71833167d8287bdf737508a5a2cd
Zaid <miqdabello21@gmail.com>
2022-12-13 01:50:32 -0800

    poll

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - printing of singledigit of base 10
+ *
+ * Return:Always return 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+printf("%d", num);
+
+printf("\n");
+
+return (0);
+}
