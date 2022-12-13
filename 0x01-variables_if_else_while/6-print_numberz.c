168d9c63a7348eeadb97d24f115fc06a931c78e3
Zaid <miqdabello21@gmail.com>
2022-12-13 01:56:29 -0800

    six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints all singledigit numbers of base 10
+ *
+ * Return:Always return 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+
+return (0);
+}
