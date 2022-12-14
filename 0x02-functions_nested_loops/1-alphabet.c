ac20e226e398ebd6cbf96159c1224cde76cad488
Zaid <miqdabello21@gmail.com>
2022-12-14 03:18:17 -0800

    start

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char lc;
+
+for (lc = 'a'; lc <= 'z'; lc++)
+_putchar(lc);
+
+_putchar('\n');
+}
+
