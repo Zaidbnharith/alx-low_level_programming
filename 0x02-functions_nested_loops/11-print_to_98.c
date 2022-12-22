29e233053e843c7fafed1a07369889888bbd2ee0
Zaid <miqdabello21@gmail.com>
2022-12-22 04:38:47 -0800

    pool

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,13 +10,13 @@ void print_to_98(int n)
 if (n > 98)
 {
 while (n > 98)
-_putchar("%d, ", n--);
-_putchar("%d\n", n);
+printf("%d, ", n--);
+printf("%d\n", n);
 }
 else
 {
 while (n < 98)
-_putchar("%d\n", n++);
-_putchar("%d\n", n);
+printf("%d\n", n++);
+printf("%d\n", n);
 }
 }
